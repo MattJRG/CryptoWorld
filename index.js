3b3e8ab152d0d5d08720da257e510895fec500ed
MattJRG <mattjrg@hotmail.co.uk>
2017-08-13 15:19:59 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
+// Routes
+app.get('/', function(req, res) {
+  const dist = path.join(__dirname, '/index.html');
+  res.sendFile(dist);
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+// Listen
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log('Listening on localhost:'+ port);
